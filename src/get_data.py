603f2e966fd1592334f48443e44c4bb9cf184150
appollo30 <leo.lasnier@me.com>
2024-11-21 00:25:50 +0100

    refactor de get_data

--- src/get_data.py
+++ src/get_data.py
@@ -1,12 +1,24 @@
 """
-Module pour télécharger des données depuis une URL et les enregistrer
-dans data/raw/
+Script pour télécharger les données brutes depuis les sources
+(voir le fichier config/sources.json)
 """
 import os
+import time
 import json
 import requests
 
-RAW_DATA_PATH = "../data/raw/" # Chemin du répertoire où enregistrer les données brutes
+RAW_DATA_PATH = "./data/raw/" # Chemin du répertoire où enregistrer les données brutes
+
+# Chemin du fichier de configuration des sources pour les urls
+SOURCES_PATH = "./config/sources.json"
+
+def clear_data():
+    """
+    Supprime les fichiers du répertoire ```./data/raw/```
+    """
+    for file_name in os.listdir(RAW_DATA_PATH):
+        file_path = os.path.join(RAW_DATA_PATH, file_name)
+        os.remove(file_path)
 
 def download_file(url : str, file_name : str):
     """
@@ -22,12 +34,23 @@ def download_file(url : str, file_name : str):
     with open(relative_path, "wb") as file:
         file.write(response.content)
 
-# Chemin du fichier de configuration des sources pour les urls
-SOURCES_PATH = "../config/sources.json"
+def download_all_files():
+    """
+    Télécharge tous les fichiers depuis les sources
+    
+    Le fichier ```communes-france.csv``` peut prendre un peu
+    plus de temps pour se télécharger que les autres (~1 minute)
+    """
+    with open(SOURCES_PATH, "r", encoding="utf-8") as sources_file:
+        sources = json.load(sources_file) # Liste des sources à télécharger
 
-with open(SOURCES_PATH, encoding="r") as sources_file:
-    sources = json.load(sources_file) # Liste des sources à télécharger
+    for source in sources:
+        t1 = time.time()
+        download_file(source["url"], source["name"])
+        print("Le fichier " + source["name"] + " a été téléchargé avec succès (" + str(round(time.time()-t1,3)) + "s)")
+        time.sleep(1/10)
 
-for source in sources:
-    download_file(source["url"], source["name"])
-    print("Le fichier " + source["name"] + " a été téléchargé avec succès.")
+if __name__ == "__main__":
+    clear_data()
+    download_all_files()
+    
